ef3911d6e2d60dbb8e0d93c8c1f0d5a0d964f96d
chao-ji <jic@indiana.edu>
2016-02-10 16:09:14 -0500

    Create test.h

---
+++ src/test.h
@@ -0,0 +1,6 @@
+#ifndef TEST_H
+#define TEST_H
+
+void test(char **pepseqs, double **peaklists, int peplen, int size, int *query_index, char **queryseqs, int query_size, double *conf, /* double *corr, */ int K, char *modelfile, int preccharge, int vec_len, char *outputfile);
+
+#endif
