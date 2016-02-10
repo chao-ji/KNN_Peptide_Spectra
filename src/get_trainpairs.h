d9076214d1522f29b9b06313bdc89d63c82a22a3
chao-ji <jic@indiana.edu>
2016-02-10 16:00:11 -0500

    Create get_trainpairs.h

---
+++ src/get_trainpairs.h
@@ -0,0 +1,6 @@
+#ifndef GET_TRAINPAIRS_H
+#define GET_TRAINPAIRS_H
+
+void get_trainpairs(char **seqs1, char **seqs2, label *cl, char **pepseqs, double **peaklists, int *perm, int peplen, int size, int pos_size, int neg_size, double rho_high, double rho_low, int vec_len);
+
+#endif
